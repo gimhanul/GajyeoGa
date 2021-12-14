72d7ab86ef1f775ce32806d412a070b63734d24e
김한울 <80656733+gimhanul@users.noreply.github.com>
2021-12-14 21:21:04 +0900

    New: sort util

---
+++ app/util/quickSort.py
@@ -0,0 +1,9 @@
+def quickSort(list):
+    if len(list) <= 1: return list
+    pivot = list[len(list) // 2].studenNumber
+    lessBay, equalBay, bigBay = [], [], []
+    for i in list:
+        if i.studentNumber < pivot: lessBay.append(i)
+        elif i.studentNumber > pivot: bigBay.append(i)
+        else: equalBay.append(i)
+    return quickSort(lessBay) + equalBay + quickSort(bigBay)
