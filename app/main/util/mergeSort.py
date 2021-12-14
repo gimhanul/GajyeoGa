b171749c981a66c882c4dbf4799201937c7b3cdb
김한울 <80656733+gimhanul@users.noreply.github.com>
2021-12-14 21:40:20 +0900

    Reupload sort util

---
+++ app/main/util/mergeSort.py
@@ -0,0 +1,27 @@
+def mergeSort(list):
+    if len(list) <= 1:
+        return list
+    mid = len(list) // 2
+    leftList = list[:mid]
+    rightList = list[mid:]
+    leftList = mergeSort(leftList)
+    rightList = mergeSort(rightList)
+    return merge(leftList, rightList)
+
+def merge(left, right):
+    result = []
+    while len(left) > 0 or len(right) > 0:
+        if len(left) > 0 and len(right) > 0:
+            if left[0].name <= right[0].name:
+                result.append(left[0])
+                left = left[1:]
+            else:
+                result.append(right[0])
+                right = right[1:]
+        elif len(left) > 0:
+            result.append(left[0])
+            left = left[1:]
+        elif len(right) > 0:
+            result.append(right[0])
+            right = right[1:]
+    return result
